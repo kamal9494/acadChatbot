f37f2cd0be4d19af663afdc042554294ce8a1e7a
KAMAL <kamaltejp@gmail.com>
2023-07-29 04:54:16 +0530

    updated firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3z7wXTS5oT_xRdnrONR2yOOx0FMQnymw",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "acadbot-407eb.firebaseapp.com",
-  projectId: "acadbot-407eb",
+  projectId: process.env.REACT_APP_P_ID,
   storageBucket: "acadbot-407eb.appspot.com",
-  messagingSenderId: "574677625467",
-  appId: "1:574677625467:web:864eb242ab3d377b5b90de",
-  measurementId: "G-N2SHMTS3BR",
+  messagingSenderId: process.env.REACT_APP_S_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_M_ID,
 };
 
 const app = initializeApp(firebaseConfig);
